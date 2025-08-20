edac4adfa433aa01db8b691a36e81a73c478f3b4
GoldenPlanetTeam <yoomyoomyoomrich@gmail.com>
2025-08-20 14:19:10 +0300

    data.js

---
+++ data.js
@@ -0,0 +1,65 @@
+const POP_DATA = {
+  "folders": [
+    {
+      "id": "school",
+      "name": "School",
+      "image": "https://img.icons8.com/color/96/school-building.png",
+      "vocabs": [
+        {
+          "image": "https://img.icons8.com/color/96/book.png",
+          "en": "Book",
+          "ar": "كتاب"
+        },
+        {
+          "image": "https://img.icons8.com/color/96/pencil.png",
+          "en": "Pencil",
+          "ar": "قلم رصاص"
+        }
+      ],
+      "verbs": [
+        {
+          "image": "https://img.icons8.com/color/96/write.png",
+          "en": "Write",
+          "ar": "يكتب"
+        },
+        {
+          "image": "https://img.icons8.com/color/96/read.png",
+          "en": "Read",
+          "ar": "يقرأ"
+        }
+      ],
+      "expressions": [
+        {
+          "image": "https://img.icons8.com/color/96/idea.png",
+          "en": "Pay attention",
+          "ar": "انتبه"
+        }
+      ]
+    },
+    {
+      "id": "animals",
+      "name": "Animals",
+      "image": "https://img.icons8.com/color/96/dog.png",
+      "vocabs": [
+        {
+          "image": "https://img.icons8.com/color/96/cat.png",
+          "en": "Cat",
+          "ar": "قط"
+        },
+        {
+          "image": "https://img.icons8.com/color/96/lion.png",
+          "en": "Lion",
+          "ar": "أسد"
+        }
+      ],
+      "verbs": [
+        {
+          "image": "https://img.icons8.com/color/96/run.png",
+          "en": "Run",
+          "ar": "يجري"
+        }
+      ],
+      "expressions": []
+    }
+  ]
+};
