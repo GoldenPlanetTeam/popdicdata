b440d0b1bc0a3ef2dda054494a8072c0ef710dbc
GoldenPlanetTeam <yoomyoomyoomrich@gmail.com>
2025-09-13 13:27:59 +0300

    data.js

--- data.js
+++ data.js
@@ -1,5 +1,72 @@
 const POP_DATA = {
   "folders": [
+  {
+    "id": "uiyjtrgedwqq1ub yhgtfdtdrhtrfuhtfruhruy6trfu6truy6fru6123456789012345678923456789456789045678945678fghjkl;ertyuiofgbhnm,krtyuirtyuiorftgyhuirtyu3456789",
+    "name": "1.1  9 global english",
+    "image": "https://giasutienganhhanoi.com/wp-content/uploads/2023/02/Cambridge-9-Global-English-LB.png",
+    "vocabs": [
+      [
+  {
+    "image": "https://img.icons8.com/color/96/chat.png",
+    "en": "Interrupt",
+    "ar": "تتكلم بينما شخص آخر يتكلم - to speak while someone else is speaking"
+  },
+  {
+    "image": "https://img.icons8.com/color/96/compare.png",
+    "en": "Compare",
+    "ar": "تنظر إلى أوجه الشبه أو الاختلاف بين الأشياء - to look at the ways things are similar or different"
+  },
+  {
+    "image": "https://img.icons8.com/color/96/distract.png",
+    "en": "Distract",
+    "ar": "تسحب انتباه شخص عن شيء ما - to take someone’s attention away from something"
+  },
+  {
+    "image": "https://img.icons8.com/color/96/idea.png",
+    "en": "Encourage",
+    "ar": "تجعل شخصًا يؤمن أنه يستطيع أن يفعل شيئًا - to make someone believe they can do something"
+  },
+  {
+    "image": "https://img.icons8.com/color/96/angry.png",
+    "en": "Tell someone off",
+    "ar": "توبّخ شخصًا لأنه فعل شيئًا خاطئًا - to speak angrily to someone because they have done something wrong"
+  },
+  {
+    "image": "https://img.icons8.com/color/96/error.png",
+    "en": "Get into trouble",
+    "ar": "تدخل في مشكلة نتيجة فعل خاطئ - to be in a situation where you have done something wrong"
+  },
+  {
+    "image": "https://img.icons8.com/color/96/share.png",
+    "en": "Share",
+    "ar": "تُطلع الآخرين على ما لديك أو تفكر فيه - to let other people know what you have"
+  },
+  {
+    "image": "https://img.icons8.com/color/96/task.png",
+    "en": "Be supposed to",
+    "ar": "من المتوقع منك أن تفعله - people expect you to do it"
+  },
+  {
+    "image": "https://img.icons8.com/color/96/focus.png",
+    "en": "Concentrate on",
+    "ar": "تركّز كل انتباهك على شيء - to give all your attention to something"
+  },
+  {
+    "image": "https://img.icons8.com/color/96/interview.png",
+    "en": "Interview",
+    "ar": "مقابلة رسمية يوجّه فيها شخص أسئلة لشخص آخر - a formal meeting in which someone asks another person questions"
+  },
+  {
+    "image": "https://img.icons8.com/color/96/news.png",
+    "en": "Journalist",
+    "ar": "شخص يعمل في كتابة أو نقل الأخبار للصحف أو التلفزيون أو الراديو - a person whose job is to write or report news for newspapers, TV, or radio"
+  }
+]
+
+    ],
+    "verbs": [],
+    "expressions": []
+  },
     {
       "id": "school",
       "name": "School",
