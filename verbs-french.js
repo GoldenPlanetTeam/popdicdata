2916f9b3d638911eaef6fadf14f19e299efe1a6b
GoldenPlanetTeam <yoomyoomyoomrich@gmail.com>
2025-09-13 13:17:15 +0300

    verbs-french.js

--- verbs-french.js
+++ verbs-french.js
@@ -1,10 +1,16 @@
   const dictionary = {
   "aimer (groupe 1)": "to love",
   "chanter (groupe 1)": "to sing",
-  "aller (groupe 1)": "to go",
+  "aller (groupe 3)": "to go",
   "jouer (groupe 1)": "to play",
   "manger (groupe 1)": "to eat",
   "parler (groupe 1)": "to speak",
-  "préférer (groupe 1)": "to prefer"
+  "préférer (groupe 1)": "to prefer",  "être (groupe 3)": "to be",
+  "avoir (groupe 3)": "to have",
+  "habiter (groupe 1)": "to live",
+  "s'appeller (groupe 1)": "to be called",
+    
+  
+
 };
 
